17bbc971fba94b594b90c56e6f567b69cc3df891
Jean Alexander Woldner <alexander@woldner.io>
2018-10-14 15:46:33 +0200

    cleanup

--- jStatusText.lua
+++ jStatusText.lua
@@ -1,60 +1,15 @@
 local AddonName, Addon = ...
 
--- locals
-local CreateFrame = CreateFrame
-local C_NamePlate = C_NamePlate
+local _G = _G
+local CreateFrame = _G.CreateFrame
+local C_NamePlate = _G.C_NamePlate
+local GetNamePlateForUnit = C_NamePlate.GetNamePlateForUnit
 
-local issecure = issecure
-local hooksecurefunc = hooksecurefunc
+local issecure = _G.issecure
+local hooksecurefunc = _G.hooksecurefunc
 
-local UnitHealth = UnitHealth
-local AbbreviateLargeNumbers = AbbreviateLargeNumbers
-
--- main
-function Addon:Load()
-  self.frame = CreateFrame("Frame", nil)
-
-  -- set OnEvent handler
-  self.frame:SetScript("OnEvent", function(handler, ...)
-      self:OnEvent(...)
-    end)
-
-  self.frame:RegisterEvent("PLAYER_LOGIN")
-  self.frame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
-end
-
--- frame events
-function Addon:OnEvent(event, ...)
-  local action = self[event]
-
-  if (action) then
-    action(self, event, ...)
-  end
-end
-
-function Addon:PLAYER_LOGIN()
-  self.frame:UnregisterEvent("PLAYER_LOGIN")
-
-  self:HookActionEvents()
-end
-
-function Addon:NAME_PLATE_UNIT_ADDED(event, unit)
-  local nameplate = C_NamePlate.GetNamePlateForUnit(unit, issecure())
-  if (not nameplate) then
-    return
-  end
-
-  local frame = nameplate.UnitFrame
-  if (not frame) then
-    return
-  end
-
-  local statusText = frame.statusText
-  if (statusText) then
-    statusText:SetPoint("LEFT", nameplate, "RIGHT", 0, -5)
-    statusText:SetVertexColor(1, 1, 1)
-  end
-end
+local UnitHealth = _G.UnitHealth
+local AbbreviateLargeNumbers = _G.AbbreviateLargeNumbers
 
 function Addon:UpdateStatusText(frame)
   local unit = frame.displayedUnit
@@ -62,7 +17,7 @@ function Addon:UpdateStatusText(frame)
     return
   end
 
-  local nameplate = C_NamePlate.GetNamePlateForUnit(unit, issecure())
+  local nameplate = GetNamePlateForUnit(unit, issecure())
   if (not nameplate) then
     return
   end
@@ -80,7 +35,6 @@ function Addon:UpdateStatusText(frame)
   statusText:SetText(AbbreviateLargeNumbers(UnitHealth(unit)))
 end
 
--- hooks
 do
   local function Frame_UpdateStatusText(frame)
     Addon:UpdateStatusText(frame)
@@ -91,5 +45,50 @@ do
   end
 end
 
--- call
+function Addon:PLAYER_LOGIN()
+  self.frame:UnregisterEvent("PLAYER_LOGIN")
+
+  self:HookActionEvents()
+end
+
+function Addon:NAME_PLATE_UNIT_ADDED(unit)
+  local nameplate = GetNamePlateForUnit(unit, issecure())
+  if (not nameplate) then
+    return
+  end
+
+  local frame = nameplate.UnitFrame
+  if (not frame) then
+    return
+  end
+
+  local statusText = frame.statusText
+  if (statusText) then
+    statusText:SetPoint("LEFT", nameplate, "RIGHT")
+    statusText:SetVertexColor(1, 1, 1)
+  end
+end
+
+function Addon:OnEvent(event, ...)
+  local action = self[event]
+
+  if (action) then
+    action(self, ...)
+  end
+end
+
+do
+  local function Frame_OnEvent(frame, ...)
+    return Addon:OnEvent(...)
+  end
+
+  function Addon:Load()
+    self.frame = CreateFrame("Frame", nil)
+    self.frame:SetScript("OnEvent", Frame_OnEvent)
+
+    self.frame:RegisterEvent("PLAYER_LOGIN")
+    self.frame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+  end
+end
+
 Addon:Load()
